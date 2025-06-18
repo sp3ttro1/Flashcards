8d94fb2826867a6edca5be0d6cf1d263da7db52a
sp3ttro1 <sp3ttro1@gmail.com>
2025-06-18 15:56:43 -0400

    added flashcard component logic

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -4,4 +4,25 @@ import "./Flashcard.css";
 
 const Flashcard = () => {
     const [index, setIndex] = useState(0);
-}
+
+    const nextCard = () => {
+        const randomIndex = Math.floor(Math.random() * flashcards.length);
+        setIndex(randomIndex);
+    };
+
+    const { verb, meaning, example} = flashcards[index];
+
+    return (
+    <div className="flashcard-container">
+        <h1>🌿 Learn Spanish Verbs</h1>
+        <div className="card">
+            <h2>{verb}</h2>
+            <p><strong>Meaning:</strong> {meaning}</p>
+            <p><em>{example}</em></p>
+        </div>
+        <button onClick={nextCard}>→</button>
+    </div>
+  );
+};
+
+export default Flashcard;
